4a3b6c07c2c84d3d94842e22c0380b33e90841ea
seoyeonii <chaesy02@gmail.com>
2025-02-09 01:57:13 +0900

    [feat] 상품 리뷰 api 연동

--- midnear/src/components/Shop/ReviewImage.jsx
+++ midnear/src/components/Shop/ReviewImage.jsx
@@ -1,56 +1,40 @@
-import React, {useState} from 'react'
-import { useNavigate } from 'react-router-dom'
-import frontImg from '../../assets/img/product/prod1.png'
-import backImg from '../../assets/img/product/prod2.png'
+import React, {useState, useEffect} from 'react'
+import { useNavigate, useLocation} from 'react-router-dom'
+import axios from 'axios';
 import Back from '../../assets/img/product/back.svg'
 import Pagination from './Pagination';
 
 const ReviewImage = () => {
-      const [limit, setLimit] = useState(8); // 한 페이지 당 보여줄 게시물 개수, 추후에 수정
-      const [page, setPage] = useState(1); // 현재 페이지 위치
-      const offset = (page -1) * limit; // 현재 페이지의 첫 번째 게시물 오프셋
+      const DOMAIN = process.env.REACT_APP_DOMAIN;
+      const location = useLocation();
+      const totalImage = location.state?.totalImage || [];
+      const params = new URLSearchParams(location.search);
+      const productName = params.get('productName');
+      const [currPage, setCurrPage] = useState(1); // 현재 페이지 위치
       const navigate = useNavigate();
+      const [images, setImages] = useState([]);
 
-      const reviewList = [
-              { 
-                userId: "sbukkki",          
-                date: "1일 전",
-                star: "5",
-                color: "RED",
-                size: "M",          
-                image: [frontImg, backImg],
-                comment: "give me a call ~ babe babe ~~"
-              },
-              { 
-                  userId: "maedariku",          
-                  date: "1일 전",
-                  star: " ",
-                  color: "RED",
-                  size: "XL",          
-                  image: [frontImg, backImg],
-                  comment: "give me a call ~ babe babe ~~"
-                },
-                { 
-                  userId: "hangyodong",          
-                  date: "3일 전",
-                  star: " ",
-                  color: "RED",
-                  size: "M",          
-                  image: [frontImg],
-                  comment: "give me a call ~ babe babe ~~"
-                },
-                { 
-                  userId: "helloworld",          
-                  date: "3일 전",
-                  star: " ",
-                  color: "RED",
-                  size: "M",          
-                  image: [frontImg, backImg,frontImg, backImg,frontImg, backImg,frontImg, backImg,frontImg, backImg],
-                  comment: "give me a call ~ babe babe ~~"
-                },
-            ]
-            const allImages = reviewList.flatMap(review => review.image);
-
+      const loadReviewImage = async(pageNumber = 1) => {
+        await axios.get(`${DOMAIN}/review/gathering`, {
+          params: {
+            productName: productName,
+            pageNumber,
+          },
+        })
+        .then((res) => {
+          if(res.status === 200){
+            setImages(res.data.data);
+          };
+        })
+        .catch((error) => {
+          console.error('이미지 모아보기 로드 실패:', error.response || error.message);
+        })
+      };
+    
+     useEffect(()=>{
+      loadReviewImage(currPage);
+     },[currPage, productName]);
+            
   return (
     <div className='container'>
     <div className='ReviewImage'>
@@ -64,13 +48,13 @@ const ReviewImage = () => {
 
       <div className='bottom-el'>
           <div className='all-imgList'>
-              {allImages.slice(offset, offset + limit).map((it, index)=>(
+              {images.map((it, index)=>(
                   <div key={index} className='image-box' >
                       <img src={it} className='image'/>
                   </div>
               ))}
           </div>
-          <Pagination total={allImages.length} limit={limit} page={page} setPage={setPage} />
+          <Pagination total={totalImage.length} limit={4} page={currPage} setPage={setCurrPage} />
       </div>
     </div>
     </div>
--- midnear/src/components/Shop/ShowReview.jsx
+++ midnear/src/components/Shop/ShowReview.jsx
@@ -1,17 +1,46 @@
 import React, {useState, useRef, useEffect} from 'react'
 import { Link } from 'react-router-dom'; 
-import SortMenu from './SortMenu'
-import frontImg from '../../assets/img/product/prod1.png'
-import backImg from '../../assets/img/product/prod2.png'
+import axios from 'axios';
 import emptyStar from '../../assets/img/product/star.svg'
 import fillStar from '../../assets/img/product/fillStar.svg'
 import rvDown from '../../assets/img/product/rvDown.svg'
 import rvUp from '../../assets/img/product/rvUp.svg'
 
-const ShowReview = () => {
+const ShowReview = ({productName}) => {
+  const DOMAIN = process.env.REACT_APP_DOMAIN;
   const [isSelected, setIsSelected] = useState(null);   
   const ref = useRef(null);
   const [isMobile, setIsMobile] = useState(false);
+  const [reviewObj, setReviewObj] = useState({});
+  const [reviews, setReviews] = useState([]);
+  const [allImages,setAllImages] = useState([]);
+  const today = new Date();
+
+  const loadReview = async() => {
+    const encodedProductName = encodeURIComponent(productName);
+    await axios.get(`${DOMAIN}/review/byProduct`, {
+      params: {
+        productName: encodedProductName,
+      },
+    })
+    .then((res) => {
+      if(res.status === 200){
+        setReviewObj(res.data.data);
+        setReviews(res.data.data.reviewList);
+        setAllImages(res.data.data.allReviewImages);
+        console.log(productName);
+        console.log('리뷰 리스트',res.data.data.reviewList);
+      };
+    })
+    .catch((error) => {
+      console.log(productName);
+      console.error('리뷰 로드 실패:', error.response || error.message);
+    })
+  };
+
+ useEffect(()=>{
+  loadReview();
+ },[productName]);
   
   useEffect(()=>{
       const checkMax =() => {
@@ -40,39 +69,18 @@ const ShowReview = () => {
     }, []);
 
     const transUserId = (userId) => {    
-      const firstPart = userId.slice(0, 3); // 앞 3글자
-      const lastPart = userId.slice(-2);    // 뒤 2글자
-      const maskedPart = '*'.repeat(userId.length - 5); // 가운데 *로 채우기
-    
-      return `${firstPart}${maskedPart}${lastPart}`;
+      const firstPart = userId.slice(0, 3); 
+      const lastPart = '*'.repeat(userId.length - 3);  
+      return `${firstPart}${lastPart}`;
     };
+    const topImage = allImages.length > 5 ? allImages.slice(0, 4) : allImages;
+    const mobileTopImage = allImages.length > 3 ? allImages.slice(0,2) : allImages;
 
-     const reviewList = [
-        { 
-          userId: "sbukkki",          
-          date: "1일 전",
-          star: 5,
-          color: "RED",
-          size: "M",          
-          image: [frontImg, backImg,frontImg, backImg,frontImg],
-          comment: "give me a call ~ babe babe ~~",
-          owners: ["감사합니다람쥐"]
-        },
-        { 
-            userId: "maedariku",          
-            date: "1일 전",
-            star: 3,
-            color: "RED",
-            size: "XL",          
-            image: [frontImg, backImg],
-            comment: "give me a call ~ babe babe ~~",
-            owners: []
-          },
-      ]
-      const allImages = reviewList.flatMap(review => review.image);
-      const topImage = allImages.length > 5 ? allImages.slice(0, 4) : allImages;
-      const mobileTopImage = allImages.length > 3 ? allImages.slice(0,2) : allImages;
-
+    const diffDays = (createdDate) => {
+      const created = new Date(createdDate);
+      const diffTime = today - created;
+      return Math.floor(diffTime / (1000 * 60 * 60 * 24));
+    };
   return (
     <div className='ShowReview'>
         <p className='title'>상품리뷰</p>
@@ -82,24 +90,24 @@ const ShowReview = () => {
                 {topImage.map((item, index)=>(
                   <img key={index} src={item} className='userImg'/>
                 ))}
-                {allImages.length > 5 && (
-                <Link to=  "/review/images">
+                {reviewObj.imageReviewCount > 5 && (
+                <Link to={`/review/images?productName=${productName}`} state={{totalImage: allImages}}>
                   <div className='moreImage'>
-                      {allImages.length - 4}개<br />더보기
+                      {reviewObj.imageReviewCount - 4}개<br />더보기
                   </div>
                 </Link>
                 )}
               </>
             )}
-            {isMobile && allImages.length > 3 && (
+            {isMobile && reviewObj.imageReviewCount > 3 && (
               <>
                 {mobileTopImage.map((item, index)=>(
                   <img key={index} src={item} className='userImg'/>
                 ))}
-                {allImages.length > 3 && (
-                <Link to=  "/review/images">
+                {reviewObj.imageReviewCount > 3 && (
+                <Link to={`/review/images?productName=${productName}`} state={{totalImage: allImages}}>
                   <div className='moreImage'>
-                      {allImages.length - 2}개<br />더보기
+                  {reviewObj.imageReviewCount - 2}개<br />더보기
                   </div>
                 </Link>
                 )}
@@ -107,23 +115,28 @@ const ShowReview = () => {
             )}
         </div>
         <div className='top-el'>
-            <p className='number'>총 {reviewList.length}건</p>
+            <p className='number'>총 {reviewObj.reviewCount}건</p>
             <div className='sort'>
-                <SortMenu />
+                <p>최신순</p>
             </div>
         </div>
-        {reviewList.map((item, index)=>(
+        {reviewObj.reviewCount === 0 ? (
+          <p>리뷰가 없습니다.</p>
+        ) : (
+            <>
+            
+        {reviews.map((item, index)=>(
             <div className='review' key={index}>
                 <div className='horizon'></div>
                 <div className='nickname'>
-                   <p>{transUserId(item.userId)}</p>
-                   <p>{item.date}</p>
+                   <p>{transUserId(item.id)}</p>
+                   <p>{diffDays(item.created)}일 전</p>
                 </div>
                 <div className='star'>
-                  {Array.from({length: item.star}).map((_,index)=>
+                  {Array.from({length: item.rating}).map((_,index)=>
                   <img src={fillStar} key={index} />
                   )}
-                  {Array.from({length: 5 - item.star}).map((_,index)=>
+                  {Array.from({length: 5 - item.rating}).map((_,index)=>
                   <img src={emptyStar} key={index} />
                   )}
                 </div>
@@ -134,8 +147,8 @@ const ShowReview = () => {
                    <p>구매</p>
                 </div>
                 <div className='userImgList'>
-                   {item.image.map((_, index)=>(
-                    <img src={item.image[index]} key={index} className='userImg'/>
+                   {item.imagesPerReview.map((_, index)=>(
+                    <img src={item.imagesPerReview[index]} key={index} className='userImg'/>
                    ))}
                 </div>
                 <div className='comment'>
@@ -143,8 +156,8 @@ const ShowReview = () => {
                 </div>
                 <div className='ownersComment' ref={ref}>
                   <div className='top'>
-                  <p>댓글 {item.owners.length}</p>
-                  {item.owners.length > 0 && (
+                  <p>댓글 {item.isReply}</p>
+                  {item.isReply > 0 && (
                     <div>
                     <img src={isSelected === item ? rvUp : rvDown} onClick={() => showComment(item.userId)} />
                    
@@ -153,12 +166,14 @@ const ShowReview = () => {
                   </div>
                   <div className='bottom'>                    
                    {isSelected === item.userId && (
-                      <p>관리자<br/>{item.owners[0]}</p>
+                      <p>관리자<br/>{item.comment}</p>
                     )}
                     </div>
                 </div>
             </div>
-        ))}
+          ))}
+        </>
+        )}
         
     </div>
   )
