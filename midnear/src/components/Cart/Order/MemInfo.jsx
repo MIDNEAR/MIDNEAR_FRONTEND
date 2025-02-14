75bd8decd683951f48854f01df69a37ac8aaa586
seoyeonii <chaesy02@gmail.com>
2025-02-14 19:24:55 +0900

    [feat] 회원 배송 api 연동

--- midnear/src/components/Cart/Order/MemInfo.jsx
+++ midnear/src/components/Cart/Order/MemInfo.jsx
@@ -1,34 +1,37 @@
 import React, { useState, useEffect } from 'react'
 import { Link, useLocation } from 'react-router-dom'
+import axios from 'axios'
 import OrderList from '../OrderList'
 import StepHeader from '../StepHeader'
 import Coupon from './Coupon'
 
 const MemInfo = () => {
+  const DOMAIN = process.env.REACT_APP_DOMAIN;
+  const token = localStorage.getItem('jwtToken');
   const location = useLocation();
-  const items = location.state?.items || []; // prodList에서 선택한(전체) 상품 받아오기
+  const orderDTO = location.state?.orderDTO || []
+  const [updateOrderDTO, setUpdateOrderDTO] = useState(orderDTO);
 
+  const [defaultAddr, setDefaultAddr] = useState(null);
   const [name, setName] =useState('');
   const [phone, setPhone] = useState('');
   const [email, setEmail] = useState('');
   const [memo, setMemo] = useState('');
+  const [zonecode, setZonecode] = useState(''); // 우편번호
  
-  const [userPoint, setUserPoint] = useState(10000);
+  const [userPoint, setUserPoint] = useState(0);
   const [inputValue, setInputValue] = useState('');
   const [error, setError] = useState('');
   const [isValidate, setIsValidate] = useState(false);
+  const [validatedPoint, setValidatedPoint] = useState(0); 
 
   const [isButtonEnabled, setIsButtonEnabled] = useState(false);
   const [selectedCoupon, setSelectedCoupon] = useState(null);
+  const [couponList, setCouponList] = useState([]);
+  const [deliveryFee, setDeliveryFee] = useState(0);
 
   const handleSelectCoupon = (coupon) => setSelectedCoupon(coupon);
   const handleResetCoupon = () => setSelectedCoupon(null);
-  
-  const couponList = [
-    {id: 1, name: '고객 감사 깜짝 20% 할인 쿠폰', sale: 20},
-    {id: 2, name: '10% 할인 쿠폰', sale: 10},
-    {id: 3, name: '30% 할인 쿠폰', sale: 30},
-  ];
 
   const hadleIsValidate = (e) => {
     const userInput = e.target.value;
@@ -42,14 +45,16 @@ const MemInfo = () => {
     }
     else if(userInput > userPoint){
       setError('보유 포인트를 초과하였습니다');
-
+      setIsValidate(true);
     }
-  }
+    else {
+      setValidatedPoint(Number(userInput));
+    }
+  };
   
-  const handleUseAllPoints = () => {
-    
-    const inputToNum = Number(userPoint);
-    setInputValue(inputToNum);
+  const handleUseAllPoints = () => {   
+    setInputValue(userPoint); 
+    setValidatedPoint(userPoint);
     setError('');
   };
 
@@ -58,6 +63,144 @@ const MemInfo = () => {
     const areFieldsFilled = name.trim() !== '' && phone.trim() !== '';
     setIsButtonEnabled(isInputValid && areFieldsFilled);
   }, [name, phone, inputValue]);
+
+  const handlePhoneChange = (e) => {
+    let input = e.target.value.replace(/[^0-9]/g, '');
+    if (input.length > 3 && input.length <= 7) {
+        input = `${input.slice(0, 3)}-${input.slice(3)}`;
+    } else if (input.length > 7) {
+        input = `${input.slice(0, 3)}-${input.slice(3, 7)}-${input.slice(7)}`;
+    }
+    setPhone(input);
+  };
+  const formatPhoneNumber = (phoneNumber) => {
+    if (/^\d{11}$/.test(phoneNumber)) {
+      return phoneNumber.replace(/(\d{3})(\d{4})(\d{4})/, '$1-$2-$3');
+    }
+    return phoneNumber;
+  };
+
+  // 기본 배송지 가져오기
+  const loadDefaultAddr = () => {
+    axios.get(`${DOMAIN}/delivery/getDefaultAddr`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((res) => {
+      if(res.status === 200){        
+        setDefaultAddr(res.data.data);
+        if(res.data.data !== null){ 
+        setZonecode(res.data.data.postalCode);
+        if(res.data.data.deliveryRequest !== null){
+          setMemo(res.data.data.deliveryRequest);
+        };
+      }
+        console.log('기본 배송지 로드 성공:', res.data.data);
+      };
+    })
+    .catch((error) => {
+      console.error('기본 배송지 로드 실패:',error.response ||  error.message);
+    });
+  };
+  // 포인트 총량 가져오기
+  const loadTotalPoint = () => {
+    axios.get(`${DOMAIN}/userCouponPoint/points/total`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((res) => {
+      if(res.status === 200){
+        setUserPoint(res.data.data);
+      };
+    })
+    .catch((error) => {
+      console.error('포인트 조회 실패:',error.response ||  error.message);
+    });
+  };
+  // 쿠폰 리스트 가져오기
+  const loadCouponList = () => {
+    axios.get(`${DOMAIN}/userCouponPoint/coupons`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((res) => {
+      if(res.status === 200){
+        setCouponList(res.data.data);
+      };
+    })
+    .catch((error) => {
+      console.error('쿠폰 조회 실패:',error.response ||  error.message);
+    });
+  };
+
+  useEffect(() => {
+    loadDefaultAddr();
+    loadCouponList();
+    loadTotalPoint();
+  }, [token]);
+
+  const calProductPrice = (start, end, price, discountPrice) => {
+    const today = new Date(); 
+    const startDate = start ? new Date(start) : null;
+    const endDate = end? new Date(end) : null;
+    if (startDate && endDate && today >= startDate && today <= endDate) {
+      return discountPrice;
+    }
+    return price;
+  };
+  const calAllPayment = (updateOrderDTO) => {
+    const totalPrice = updateOrderDTO.reduce((acc, item) => {
+      const productPrice = calProductPrice(item.discountStartDate, item.discountEndDate, item.price, item.discountPrice);
+      const totalItemPrice = productPrice * item.quantity;
+      return acc + totalItemPrice; 
+    }, 0);
+    return totalPrice;
+  };
+
+  const calFinalPayment = (updateOrderDTO, selectedCoupon, validatedPoint) => {
+    const totalPrice = calAllPayment(updateOrderDTO);
+    const couponDiscount = selectedCoupon ? (100 - selectedCoupon.discountRage) / 100 : 1;
+    const finalPrice = totalPrice * couponDiscount - validatedPoint;
+
+    return finalPrice;
+  };
+
+  // 회원 주문
+  const userOrder = () => {
+    const formattedPhone = phone.replace(/-/g, ''); 
+    axios.post(`${DOMAIN}/orders/create`,{
+        orderName: name,
+        orderContact: formattedPhone,
+        orderEmail: email,
+        allPayment: calFinalPayment(updateOrderDTO, selectedCoupon, validatedPoint), 
+        deliveryAddrId: defaultAddr.deliveryAddressId,
+        deliveryRequest: memo,
+        userCouponId: selectedCoupon.userCouponId,
+        oderProductsRequestDtos: updateOrderDTO.map(item => {
+          const productPrice = calProductPrice(item.discountStartDate, item.discountEndDate, item.price, item.discountPrice);
+          return { 
+              productColorId: item.productColorId,
+              size: item.size,
+              quantity: item.quantity,
+              couponDiscount: productPrice * (selectedCoupon.discountRage / 100), 
+              pointDiscount: validatedPoint / updateOrderDTO.length,
+              productPrice: productPrice,
+              deliveryCharge: deliveryFee,
+          };
+        })
+    })
+    .then((res) => {
+      if(res.status === 200){
+        console.log('주문 성공:', res.data.data);
+      };
+    })
+    .catch((error) => {
+      console.error('주문 실패:', error.message);
+    });
+  };
 
   return (
     <div className='member'>
@@ -71,35 +214,35 @@ const MemInfo = () => {
                 <div className='mid_text'>주문자 정보</div>
                 <p className='min_text_ex'>별표(*)로 표시된 필드가 필수 필드입니다.</p>
 
-                <input type='text' name='name' className='min_text' placeholder='홍길동*' onChange={(e) => setName(e.target.value)}/>
-                <input type='text' name='phone' className='min_text' placeholder='010-9999-9999*'  onChange={(e) => setPhone(e.target.value)}/>
-                <input type='text' name='email' className='min_text' placeholder='이메일' onChange={(e) => setEmail(e.target.value)}/>
+                <input type='text' name='name' className='min_text' placeholder='홍길동*' value={name} onChange={(e) => setName(e.target.value)}/>
+                <input type='text' name='phone' className='min_text' placeholder='010-9999-9999*' value={phone} onChange={handlePhoneChange}/>
+                <input type='text' name='email' className='min_text' placeholder='이메일' value={email} onChange={(e) => setEmail(e.target.value)}/>
 
                 <div className='bottom'>
                   <div className='title'>배송 정보</div>
-                  {/** 기본 배송지 없을 때 
-                  <p className='min_text_ex'>입력된 배송 정보가 존재하지 않습니다.</p>
-                  <Link to='/order/delivery/new-address'><button className='add-btn'>배송 정보 추가하기</button></Link>
-                  */}
-                  {/** 기본 배송지 있을 때*/}
-                  <div className='default_add'>
-                    <div className='userInfo'>
-                      <div>
-                        <p className='b_txt'>홍길동</p>
-                        <p className='g_txt'>010-1233-1222</p>
+                  {defaultAddr === null ? (
+                    <>
+                    <p className='min_text_ex'>입력된 배송 정보가 존재하지 않습니다.</p>
+                    <Link to='/order/delivery/new-address'><button className='add-btn'>배송 정보 추가하기</button></Link>
+                    </>
+                  ) : (                  
+                    <div className='default_add'>
+                      <div className='userInfo'>
+                        <div>
+                          <p className='b_txt'>{defaultAddr.recipient}</p>
+                          <p className='g_txt'>{formatPhoneNumber(defaultAddr.recipientContact)}</p>
+                        </div>
+                        <Link to='/order/delivery/select-address'><div className='change-btn'>변경</div></Link>
                       </div>
-                      <Link to='/order/delivery/select-address'><div className='change-btn'>변경</div></Link>
-                    </div>
-                    
-                    <p className='b_txt'>서울특별시 서대문구 성산로 8길 9-9 (연희동)</p>
-                    <p className='b_txt'>ㅇㅇ아파트 109동 109호(종 1234)</p>
-                    <p className='g_txt'>(123098)</p>
-                    <p className='m_txt'>배송메모</p>
-                    <input type='text' name='memo' className='memo'  placeholder='배송 메모를 입력해 주세요.' onChange={(e) => setMemo(e.target.value)}/>
-                    <div>
-
-                    </div>
-                  </div> 
+                      
+                      <p className='b_txt'>{defaultAddr.address}</p>
+                      <p className='b_txt'>{defaultAddr.detailAddress}</p>
+                      <p className='g_txt'>({defaultAddr.postalCode})</p>
+                      <p className='m_txt'>배송메모</p>
+                      <input type='text' name='memo' className='memo'  placeholder='배송 메모를 입력해 주세요.' value={memo} onChange={(e) => setMemo(e.target.value)}/>
+                      
+                    </div> 
+                  )}
 
                 </div>
                 <div className='point'>
@@ -117,22 +260,27 @@ const MemInfo = () => {
                 </div>
 
                
-                <Link to='/order/pay-succeed' className='pay-link'>
+                {/*<Link to='/order/pay-succeed' className='pay-link'>*/}
                     <button 
                      className={`btn ${isButtonEnabled ? 'enabled' : 'disabled'}`}
                      disabled={!isButtonEnabled}
                      >
                       결제하기</button>
-                </Link>
+                {/*</Link>*/}
         </div>
 
           <div className='order_content'>
             <div className='title'>주문 내용</div>
             <div className='s_title'>상품</div>
             <OrderList 
-              productList={items}
-              point={inputValue || 0}
+              productList={orderDTO}
+              point={validatedPoint || 0}
               selectedCoupon={selectedCoupon}
+              postCode = {zonecode || 0}
+              isCartScreen={false}
+              setUpdateOrderDTO={setUpdateOrderDTO}
+              setDeliveryFee={setDeliveryFee}
+              deliveryFee={deliveryFee}
              />
           </div>
         </div>
--- midnear/src/components/Cart/Order/NewAddress.jsx
+++ midnear/src/components/Cart/Order/NewAddress.jsx
@@ -1,20 +1,59 @@
 import React, {useState, useEffect} from 'react'
 import { useNavigate, Link } from 'react-router-dom'
+import {useDaumPostcodePopup}  from 'react-daum-postcode';
+import axios from 'axios';
 import PrivacyModal from './PrivacyModal'
 import check from '../../../assets/img/cart/check.svg'
 import StepHeader from '../StepHeader'
 
-const NewAddress = () => {    
+const NewAddress = () => {       
+    const DOMAIN = process.env.REACT_APP_DOMAIN;
+    const token = localStorage.getItem('jwtToken');
+    
     const navigate = useNavigate();
     const [isModalOpen, setIsModalOpen] = useState(false);    
     const openModal = () => setIsModalOpen(true);
     const closeModal = () => setIsModalOpen(false);
     const [isButtonEnabled, setIsButtonEnabled] = useState(false);
+    
     const [receiver, setReceiver] = useState('');
     const [receiverNum, setReceiverNum] = useState('');
     const [destination, setDestination] = useState('');
+    const [zonecode, setZonecode] = useState(''); 
+    const [address, setAddress] = useState('');
+    const [detailAddress, setDetailAddress] = useState('');
+    const [memo, setMemo] = useState('');
     const [isChecked, setIsChecked] = useState([]);
-    
+    const [defaultAddressStatus, setDefaultAddressStatus] = useState(0);
+
+    const postcodeScriptUrl =
+        "https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js";
+    const open = useDaumPostcodePopup(postcodeScriptUrl);
+    const completeHandler = (data) => {
+        const { address,zonecode, bname, buildingName } = data;
+        let extraAddress = "";
+        let fullAddress = "";
+        if(data.addressType === "R"){
+            if(bname !== ""){
+                extraAddress += bname;
+            };
+            if(buildingName !== ""){
+                if(extraAddress !== ""){
+                    extraAddress += `,${buildingName}`;
+                } else {extraAddress += buildingName;}
+            };
+            if(extraAddress !== ""){
+                fullAddress = address + `(${extraAddress})`;
+            }
+        }
+        setZonecode(zonecode);
+        setAddress(fullAddress);
+    };
+        
+    const toggleHandler = () => {
+        open({ onComplete: completeHandler });
+    };
+
     const selectAdd = () => {
         navigate('/order/delivery/select-address');
     };
@@ -24,21 +63,64 @@ const NewAddress = () => {
     };
 
     useEffect(() => {
-        const areFieldsFilled = receiver.trim() !== '' && receiverNum.trim() !== '' && receiver.trim() !== '' && receiverNum.trim() !== '';
+        const areFieldsFilled = receiver.trim() !== '' && receiverNum.trim() !== ''  && zonecode.trim() !== ''  && address.trim() !== '';
         const isPrivacyChecked = isChecked.includes(2);
         setIsButtonEnabled(areFieldsFilled && isPrivacyChecked);
-    }, [receiver, receiverNum, isChecked]);
+    }, [receiver, receiverNum, zonecode, address, isChecked]);
 
     const checkItemHandler = (id, isChecked) => {
         setIsChecked((prev) =>
             isChecked ? [...prev, id] : prev.filter((itemId) => itemId !== id)
           );
-      };
+    };
     
-      const isCheckItem = (e, id) => {
-        const { checked } = e.target;
-        checkItemHandler(id, checked);
-      };
+    const isCheckItem = (e, id) => {
+      const { checked } = e.target;
+      if (id === 1) {
+        setDefaultAddressStatus(checked ? 1 : 0); 
+      }
+      checkItemHandler(id, checked);
+    };
+
+    const handleReceiverNumChange = (e) => {
+        let input = e.target.value.replace(/[^0-9]/g, '');
+        if (input.length > 3 && input.length <= 7) {
+            input = `${input.slice(0, 3)}-${input.slice(3)}`;
+        } else if (input.length > 7) {
+            input = `${input.slice(0, 3)}-${input.slice(3, 7)}-${input.slice(7)}`;
+        }
+        setReceiverNum(input);
+    };
+
+    // 배송지 신규 입력
+    const postNewAddr = () => {
+        const formattedReceiverNum = receiverNum.replace(/-/g, ''); 
+        axios.post(`${DOMAIN}/delivery/createAddr`,{
+            deliveryAddressId: 0,
+            recipient: receiver,
+            recipientContact: formattedReceiverNum,
+            deliveryName: destination,
+            postalCode: zonecode,
+            address: address,
+            detailAddress: detailAddress,
+            defaultAddressStatus: defaultAddressStatus,
+            deliveryRequest: memo,
+            userId: 0,
+        },
+        {
+            headers: {
+                Authorization: `Bearer ${token}`,
+              },
+        })
+        .then((res) => {
+          if(res.status === 200){
+            console.log('배송지 등록 성공:', res.data.data);
+          };
+        })
+        .catch((error) => {
+          console.error('배송지 등록 실패:', error.message);
+        });
+    };
 
   return (
     <div className='address'>
@@ -57,17 +139,17 @@ const NewAddress = () => {
             </div>
 
             <div className='bottom'>
-                <input type='text' name='receiver' className='min_text' placeholder='수취인명*'  onChange={(e) => setReceiver(e.target.value)}/>
-                <input type='text' name='receiverNum' className='min_text' placeholder='수취인 번호*' onChange={(e) => setReceiverNum(e.target.value)}/>
-                <input type='text' name='destination' className='min_text' placeholder='배송지명' onChange={(e) => setDestination(e.target.value)} />
+                <input type='text' name='receiver' className='min_text' placeholder='수취인명*' value={receiver} onChange={(e) => setReceiver(e.target.value)}/>
+                <input type='text' name='receiverNum' className='min_text' placeholder='수취인 번호*' value={receiverNum}  onChange={handleReceiverNumChange}/>
+                <input type='text' name='destination' className='min_text' placeholder='배송지명' value={destination} onChange={(e) => setDestination(e.target.value)} />
                 <div className='post_code'>
-                    <input type='text' name='postNum' className='text' placeholder='*우편번호' disabled />
-                    <div className='search'>우편번호 검색</div>
+                    <input type='text' name='postNum' className='text' placeholder='*우편번호' disabled value={zonecode}/>
+                    <div className='search'  onClick={toggleHandler}>우편번호 검색</div>
                 </div>
-                <input type='text' name='firstAdd' className='min_text' placeholder='*주소' disabled/>
-                <input type='text' name='secondAdd' className='min_text' placeholder='상세주소 입력' />
+                <input type='text' name='firstAdd' className='min_text' placeholder='*주소' disabled value={address}/>
+                <input type='text' name='secondAdd' className='min_text' placeholder='상세주소 입력' value={detailAddress} onChange={(e) => setDetailAddress(e.target.value)}/>
                 
-                <input type='text' name='memo' className='min_text' placeholder='배송 메모를 입력해 주세요.' />
+                <input type='text' name='memo' className='min_text' placeholder='배송 메모를 입력해 주세요.' value={memo}  onChange={(e) => setMemo(e.target.value)}/>
             </div>
                 
             <div className='check'>
@@ -98,12 +180,13 @@ const NewAddress = () => {
                 <PrivacyModal isOpen={isModalOpen} closeModal={closeModal} setIsChecked={setIsChecked} isChecked={isChecked}  />
             </div>
             <Link to='/order/delivery/select-address'>
-            <button 
-            className={`btn ${isButtonEnabled ? 'enabled' : 'disabled'}`}
-            disabled={!isButtonEnabled}
-            >
-                저장
-            </button>
+                <button 
+                className={`btn ${isButtonEnabled ? 'enabled' : 'disabled'}`}
+                disabled={!isButtonEnabled}
+                onClick={postNewAddr}
+                >
+                    저장
+                </button>
             </Link>
         </div>
         
