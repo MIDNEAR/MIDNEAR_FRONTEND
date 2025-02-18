592e154cccea5243bf03318a87d51d7460fbdc1e
seoyeonii <chaesy02@gmail.com>
2025-02-18 19:23:21 +0900

    [feat]  마이페이지 쿠폰 포인트 api 연동

--- midnear/src/components/Mypage/Cupon/Colligation.jsx
+++ midnear/src/components/Mypage/Cupon/Colligation.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import MyPageModal from '../MyPageModal'
+import axios from 'axios'
 
 const Colligation = () => {
+  const DOMAIN = process.env.REACT_APP_DOMAIN;
+  const token = localStorage.getItem('jwtToken');
+  const [couponNum, setCouponNum] = useState(0);
+  const [point, setPoint] = useState(0);
+  const loadCouponNum = () => {
+    axios.get(`${DOMAIN}/userCouponPoint/coupons/count`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((res) => {
+      if(res.status === 200){
+        setCouponNum(res.data.data);
+      };
+    })
+    .catch((error) => {
+    });
+  };
+  const loadPoint = () => {
+    axios.get(`${DOMAIN}/userCouponPoint/points/total`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((res) => {
+      if(res.status === 200){
+        setPoint(res.data.data);
+      };
+    })
+    .catch((error) => {
+      
+    });
+  };
+
+    useEffect(() => {
+      loadCouponNum();
+      loadPoint();
+    }, [DOMAIN, token]);
+
   return (
     <div className="container">
       <div className="field_container">
@@ -12,11 +52,11 @@ const Colligation = () => {
                 <div className='option_box'>
                     <Link to='/mypage/colligation/point' className='detail_box'>
                         <div className='detail_box-title'>보유 적립금</div>
-                        <div className='detail_box-contents'>1,111원</div>
+                        <div className='detail_box-contents'>{point.toLocaleString('ko-KR')}원</div>
                     </Link>
                     <Link to='/mypage/colligation/cupon' className='detail_box'>
                         <div className='detail_box-title'>쿠폰</div>
-                        <div className='detail_box-contents'>3장</div>
+                        <div className='detail_box-contents'>{couponNum}장</div>
                     </Link>
                 </div>
             </div>
